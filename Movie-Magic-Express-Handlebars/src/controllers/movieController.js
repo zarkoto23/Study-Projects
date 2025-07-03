e79613035744023df54ceedf8a84374c03831182
Svetozar Stoev <zarkoto23@gmail.com>
2025-07-03 14:52:21 +0300

    Enhance search functionality to support filtering by title, genre, and year; update views to retain filter values

--- Movie-Magic-Express-Handlebars/src/controllers/movieController.js
+++ Movie-Magic-Express-Handlebars/src/controllers/movieController.js
@@ -22,8 +22,10 @@ movieController.get("/:movieId/details", (req, res) => {
 });
 
 movieController.get('/search',(req, res)=>{
-    const movies=movieService.getAll()
-    res.render('search',{movies})
+    const filter=req.query
+
+    const movies=movieService.getAll(filter)
+    res.render('search',{movies,filter})
 })
 
 export default movieController;
--- Movie-Magic-Express-Handlebars/src/services/movieService.js
+++ Movie-Magic-Express-Handlebars/src/services/movieService.js
@@ -2,8 +2,21 @@ import movies from "../movies.js";
 import { v4 as uuid } from "uuid";
 
 export default {
-  getAll() {
-    return movies;
+  getAll(filter={}) {
+    let result=movies
+        if(filter.search){
+            result=result.filter(movie=>movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+        }
+
+        if(filter.genre){
+            result=result.filter(movie=>movie.genre.toLowerCase().includes(filter.genre.toLowerCase()))
+        }
+        if(filter.year){
+            result=result.filter(movie=>movie.year==filter.year)
+        }
+
+
+    return result;
   },
 
   findMovie(movieId) {
