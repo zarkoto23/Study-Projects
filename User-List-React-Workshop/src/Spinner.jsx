81b96e95e8d903287d9d4c5fc76f20d96755c21f
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-02 16:09:49 +0300

    init html to jsx components

---
+++ User-List-React-Workshop/src/Spinner.jsx
@@ -0,0 +1,13 @@
+export default function Spinner() {
+  return(
+    
+       <div className="table-wrapper">
+         Overlap components  
+
+         <div className="loading-shade">
+        Loading spinner  
+        <div className="spinner"></div> 
+        </div>
+        </div>
+)
+}
